77e92b14b62296fcec5c8d40c0dbd4bf94ac1989
For Web <thebestweb@yandex.ru>
2021-03-11 21:55:55 +0500

    L5

--- front-page.php
+++ front-page.php
@@ -7,10 +7,12 @@
 					global $post;
 					// обновляем глобальную переменную
 					$myposts = get_posts([ 
+						// последний пост
+						// 'offset' => 1
+						// 'orderby'	=> 'javascript',
+						// 'order'   => 'ASC'	
 						'numberposts' => 1,
-						// 'orderby'	=> 'javascript',			
-						// 'category-name' => 'javascript, css, html, web-design'
-						// 'order'   => 'ASC'						
+						'category_name' => 'javascript, css, html, web-design'					
 					]);
 
 					if( $myposts ){
@@ -18,7 +20,7 @@
 							// переменную post передаем в ф-ю setup_postdata()
 							setup_postdata( $post );
 							?>
-								<!-- Вывод заголовка текущей записи -->	
+								<!-- Вывод заголовка текущей записи и миниатюры -->	
 							<img src="<?php the_post_thumbnail_url() ?>" alt=""	class="post-thumb">
 							<?php $author_id = get_the_author_meta('ID'); ?>		
 							<a href="<?php echo get_author_posts_url( $author_id); ?>" class="author">
@@ -30,7 +32,7 @@
 							</a>
 							<div class="post-text">
 								<?php the_category(); ?>
-								<h2 class="post-title"><?php the_title(); ?></h2>
+								<h2 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h2>
 								<a href="<?php echo get_the_permalink() ?>" class="more">Читать далее</a>
 							</div>
 					
@@ -51,12 +53,12 @@
 					<?php
 						global $post;
 						// обновляем глобальную переменную
-						$myposts = get_posts([ 
+						$myposts = get_posts([
+							// 'orderby'	=> 'javascript',
+							// 'order'   => 'ASC'	
 							'numberposts' => 5,
-							'offset' => 1
-							// 'orderby'	=> 'javascript',			
-							'category-name' => 'javascript, css, html, web-design'
-							// 'order'   => 'ASC'						
+							'offset' => 1,
+							'category_name' => 'javascript, css, html, web-design'					
 						]);
 
 						if( $myposts ){
@@ -68,7 +70,7 @@
 							<li class="post">
 								<?php the_category() ?>
 								<a class="post-permalink" href="<?php echo get_the_permalink(); ?>">
-									<h4 class="post-title"><?php the_title(); ?></h4
+									<h4 class="post-title"><?php echo mb_strimwidth(get_the_title(), 0, 60, '...'); ?></h4
 								</a>
 								
 							</li>
@@ -94,7 +96,45 @@
 	<!-- /.container -->
 
 </main>
+<div class="container">
+	<ul class="article-list">
+		<?php
+			global $post;
+			// обновляем глобальную переменную
+			$myposts = get_posts([ 
+				'numberposts' => 4,
+				'category_name' => 'articles'
+				// 'offset' => 1
+				// 'orderby'	=> 'javascript',			
+				// 'order'   => 'ASC'						
+			]);
 
+			if( $myposts ){
+				foreach( $myposts as $post ){
+					// переменную post передаем в ф-ю setup_postdata()
+					setup_postdata( $post );
+					?>
+				
+				<li class="article-item">	
+					<a class="article-permalink" href="<?php echo get_the_permalink(); ?>">
+						<!-- <h4 class="article-title"><?php the_title(); ?></h4> -->
+						<h4 class="article-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...'); ?></h4>
+					</a>
+					<img src="<?php echo get_the_post_thumbnail_url(null, 'homepage-thumb'); 
+					?>" alt="">			
+				</li>
+				<?php 
+				}
+				} else {
+					// Постов не найдено
+					?> <p> Постов нет</p> <?php
+
+				}
+			wp_reset_postdata(); // Сбрасываем $post
+		?>			
+	</ul>
+</div>
+<!-- /.container -->
 
 
 

832d1fac530ef877de100fe15720c4f334575002
For Web <thebestweb@yandex.ru>
2021-03-13 15:04:48 +0500

    L6

---
+++ functions.php
@@ -0,0 +1,54 @@
+<?php
+// Добавление расширенных возможностей
+if ( ! function_exists( 'universal_theme_setup' ) ) :	
+	function universal_theme_setup() {
+		// Добавление тега title-tag
+		add_theme_support( 'title-tag' );
+
+		// Добавление миниатюр - только для постов
+		add_theme_support( 'post-thumbnails', array( 'post' ) );
+
+		// Добавление пользовательского логотипа
+		add_theme_support( 'custom-logo', [			
+			'width'       => 163,			
+			'flex-height' => true,			
+			'header-text' => 'Universal',
+			'unlink-homepage-logo' => true, // WP 5.5
+		] );
+
+		// Регистрация меню
+		register_nav_menus( [
+			'header_menu' => 'Меню в шапке',
+			'footer_menu' => 'Меню в подвале'
+		] );
+	}	
+endif;
+add_action( 'after_setup_theme', 'universal_theme_setup' );
+
+// правильный способ подключить стили и скрипты
+
+function enqueue_universal_style() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_enqueue_style( 'universal-theme', get_template_directory_uri() . '/assets/css/universal-theme.css', 'style', time());	
+	// добавляем Roboto Slab
+	wp_enqueue_style('Roboto Slab', 'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+
+# отключаем создание миниатюр файлов для указанных размеров
+add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
+function delete_intermediate_image_sizes( $sizes ){
+	// размеры которые нужно удалить
+	return array_diff( $sizes, [
+		'medium_large',
+		'large',
+		'1536x1536',
+		'2048x2048',
+	] );
+}
+
+//добавление миниатюр
+
+if ( function_exists( 'add_image_size' ) ) {	
+	add_image_size( 'homepage-thumb', 65, 65, true ); // Кадрирование изображения
+}

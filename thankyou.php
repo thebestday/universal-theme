daa35e88c3b41101fabc3d76e098f56dc65d1aa3
For Web <thebestweb@yandex.ru>
2021-04-01 04:18:09 +0500

    L19

---
+++ thankyou.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Страница благодарности
+Template Post Type: page
+*/
+
+get_header();
+?>
+<div class="container">
+	<?php the_content(); ?>
+</div>
+<?php
+
+get_footer();
